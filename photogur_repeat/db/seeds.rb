aa47e2593fbf6b10113a84ce79d5bd1b8c475689
RHEA GANAPATHY <rheasg@gmail.com>
2015-06-24 17:06:27 -0400

    Add picture and links to homepage updated

--- photogur_repeat/db/seeds.rb
+++ photogur_repeat/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Image.create!(
+  :title => "Red Deck by the lake",
+  :artist => "OrlandoBloomsPretzel",
+  :url => "http://i.imgur.com/FEPDZ8M.jpg",
+  :description => "Best picture after 1 year of photography" 
+  )
+
+Image.create!(
+  :title => "The Blue Bridge",
+  :artist => "BisFitty",
+  :url => "http://i.imgur.com/SSSFI.jpg",
+  :description => "Downtown Jacksonville"
+  )
